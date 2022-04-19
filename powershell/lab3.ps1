4750abd81d873ef9481dbf6dedcf1ac533f3c975
Elvin <aliyev.elvin.89@gmail.com>
2022-04-19 00:32:38 -0400

    Create lab3.ps1

---
+++ powershell/lab3.ps1
@@ -0,0 +1,10 @@
+﻿Get-CimInstance -className Win32_NetworkAdapterConfiguration | Where-Object IPEnabled -EQ "True" |
+     foreach{
+                 New-Object -TypeName psobject -Property @{Description =$_.Description
+                                                           Index =$_.Index
+                                                           IPaddress=$_.ipaddress
+                                                           Subnetmask = $_.Ipsubnet
+                                                           DNsName=$_.dnsdomain
+                                                           DNSserver=$_.DNSServersearchorder
+                                                           }
+         }|Format-Table Description,Index,IPaddress, Subnetmask,DNsName,DNSserver
